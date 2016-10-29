95d469b306e8d80b8c85aa07bdbdf6e246defcdc
abhinav-garg <abhinav.garg@live.com>
2016-10-30 00:59:12 +0530

    Added benchmarking and plotting tools

--- insertion.py
+++ insertion.py
@@ -1,6 +1,15 @@
 from random import random as rnd
 from time import time
 import code
+import matplotlib.pyplot as plt
+
+plt.ion()
+fig, ax = plt.subplots()
+ax.set_xlabel('Input Size')
+ax.set_ylabel('Time')
+
+ax.set_xscale('log')
+ax.set_yscale('log')
 
 class InsertionSort:
 	def __init__(self, inSeq = []):
@@ -23,15 +32,38 @@ class InsertionSort:
 		self.time = t1-t0
 		return self.outSeq, self.time
 
-def evaluateAlgo(Algorithm=InsertionSort, inputSizes=[10,100,1000,10000]):
-	times = []
-	for inputSize in inputSizes:
-		inSeq = [int(rnd()*1000) for i in range(inputSize)]	# randomly generate N(inputSize) numbers 0 bw 0 & 1000
-		algo = Algorithm()
-		outSeq, time = algo.sort(inSeq)
-		times.append((inputSize, time))
-	return times
+class EvaluateAlgo:
+	def __init__(self, inputSizes=[10,100,1000], average=3):
+		self.inputSizes = inputSizes
+		self.average = average
+
+	def setParams(self, inputSizes=[10,100,1000], average=3):
+		self.inputSizes = inputSizes # Input sizes for which algo is run
+		self.average = average # Averaging over 3 runs for same size
+
+	def evaluate(self, Algorithm=InsertionSort):
+		self.algo = Algorithm()
+		self.times = []
+		for inputSize in self.inputSizes:
+			totalTime = 0
+			for i in range(self.average):
+				# randomly generate N(inputSize) numbers 0 bw 0 & 1000
+				inSeq = [int(rnd()*1000) for i in range(inputSize)]	
+				outSeq, time = self.algo.sort(inSeq)
+				totalTime += time
+			avgTime = totalTime/self.average
+			self.times.append((inputSize, avgTime))
+		return self.times
+
+	def plot(self, ax):
+		xVals, yVals = [list(t) for t in zip(*self.times)]
+		ax.plot(xVals, yVals, '-o',label=self.algo.__class__.__name__)
+		plt.legend()
+		plt.draw()
 
 if __name__ == '__main__':
-	times = evaluateAlgo()
+	evaluate = EvaluateAlgo(inputSizes=[10,100,1000,10000], average=1)
+	times = evaluate.evaluate()
+	evaluate.plot(ax)
 	print times
+	code.interact(local=dict(globals(), **locals()))
